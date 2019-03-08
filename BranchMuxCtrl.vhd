02ef66b0c58812d1ded6e3cdbda8f41cf0332963
yw540424419 <47457281+yw540424419@users.noreply.github.com>
2019-03-08 12:08:53 -0500

    Simulation Done

--- BranchMuxCtrl.vhd
+++ BranchMuxCtrl.vhd
@@ -1,18 +1,17 @@
 library ieee;
-use ieee,std_logic_1164.all;
+use ieee.std_logic_1164.all;
 
 entity BranchMuxCtrl is
 port (
 	zero,BEQ,BNE : in std_logic;
-	muxSel: out std_logic_vector(2 downto 0)
+	muxSel: out std_logic
 	
 );
 end entity;
 architecture logic of BranchMuxCtrl is
 signal branchE,branchNE:std_logic; 
 begin
-        branchE   <= beq and zero;
+        branchE   <= BEQ and zero;
 	branchNE <= BNE and (not zero);
 	muxSel    <= branchE or branchNE;
 end architecture logic;
-      
