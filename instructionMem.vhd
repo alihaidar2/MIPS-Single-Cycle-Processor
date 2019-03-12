65c5528f05a8c93a3f12968b4a18cf69438996ce
yw540424419 <47457281+yw540424419@users.noreply.github.com>
2019-03-12 19:24:57 -0400

    Update instructionMem.vhd

--- instructionMem.vhd
+++ instructionMem.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity instructionMem is
 	port (
-		pcAddIn : in std_logic_vector(7 downto 0);
+		pcAddIn : in std_logic_vector(31 downto 0);
 		instructionOut: out std_logic_vector(31 downto 0)
 	);
 end entity;
