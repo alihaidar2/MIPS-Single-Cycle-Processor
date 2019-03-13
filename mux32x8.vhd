6213acc7789f72b8fb2544175499453ba3fd1189
Boris Nz <borisnz101@gmail.com>
2019-03-13 14:51:41 -0400

    updates

--- mux32x8.vhd
+++ mux32x8.vhd
@@ -2,9 +2,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library work;
-use work.all;
-
 --8 inputs mux (32 bits)
 -- Used to output CPU's registers content:
 
@@ -16,15 +13,15 @@ use work.all;
 --Other ['0', RegDst, Jump, MemRead, The remaining
 --MemtoReg, AluOp[1..0], AluSrc] control information
 
-entity top_mux8x8 is
+entity mux32x8 is
 	
 	port(PC, ALUresult, readData1, readData2, writeData, other, i6, i7: in std_logic_vector(7 downto 0); -- i6 and i7 not used
 		sel :in std_logic_vector(2 downto 0);
 		muxOut: out std_logic_vector(7 downto 0));
 		
-end entity top_mux8x8;
+end entity mux32x8;
 
-architecture muxBehave of top_mux8x8 is
+architecture muxBehave of mux32x8 is
 	
 component mux1x8 is
 	
@@ -45,10 +42,13 @@ begin
 end muxBehave;
 
 -- Linking of components and top level entity
-configuration conf_top_mux8x8 of top_mux8x8 is
-	for muxBehave
-		for all : mux1x8
-			use entity work.mux1x8(muxBehave);
+configuration conf_mux32x8 of mux32x8 is
+	for regbehave
+		for all : d_latch
+			use entity work.d_latch(basic);
+		end for;
+		for all : and_gate2
+			use entity work.and_gate2(basic);
 		end for;
 	end for;
-end conf_top_mux8x8;
+end conf_reg8bits;
